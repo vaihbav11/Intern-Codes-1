16d4052ed565c5cc1cb3162029ff60b7df9cd272
Vaibhav Chaturvedi <110451139+vaihbav11@users.noreply.github.com>
2026-06-27 23:33:23 +0530

    Fix formatting issues in Untitled14.py

--- Untitled14.py
+++ Untitled14.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 get_ipython().run_line_magic('matplotlib', 'inline')
-
+ 
 
 # In[5]:
 
